c6233c485faf69b4412cffff019587169c9b018c
Jonah Köllner <koellnjo@gmail.com>
2022-12-14 21:23:59 +0100

    added some thoughts to the commented theory in the get_next_line.c

--- 42_Heilbronn/Basecamp/get_next_line/get_next_line.c
+++ 42_Heilbronn/Basecamp/get_next_line/get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkollner <jkollner@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jkollner <jkollner@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 09:44:13 by jkollner          #+#    #+#             */
-/*   Updated: 2022/12/14 16:41:13 by jkollner         ###   ########.fr       */
+/*   Updated: 2022/12/14 21:22:17 by jkollner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,38 @@ char	*get_next_line(int fd)
 				// error return NULL
 				// clean the buffer and clean the string
 		// check_nl() != 0
-			// return realloc(str, get_string_out(buffer, check_nl()))
-		// add to string and go to 21 and clean buffer
+			// return realloc(str, get_string(buffer, check_nl()))
+		// realloc(str, get_string(buffer, BUFFER_SIZE)) add to string and go to 21 and clean buffer to the given number
 	// if (read == -1)
 		// return NULL
 	// return string
+
+
+
+
+	// Sanity check for BUFFER_SIZE define
+	// static char *buffer witht the size of BUFFER_SIZE (calloc)
+	// define char *str
+	// check if there is something in the buffer (if buffer[0] != 0)
+		// check_nl() != 0
+			// return realloc(str, get_string_out(buffer, check_nl())
+		// add to the return string (ft_realloc_join)
+		// clean_up_buffer clean the entire buffer (just to_clean with buffersize)
+	// while ((read = read()) == amount(=BUFFER_SIZE))
+		// check read return value
+			// if return == -1
+				// error return NULL
+				// clean the buffer and clean the string
+		// check_nl() != 0
+			// return realloc(str, get_string(buffer, check_nl()))
+		// realloc(str, get_string(buffer, BUFFER_SIZE)) add to string and go to 21 and clean buffer to the given number
+	// if (read == -1)
+		// return NULL
+	// string = realloc(string, buffer); // realloc already stops joining at the first \0 it findes, so there is no need to cut the buffer
+	// clean_up_buffer()
+	// return string
+
+	// NEEDED METHODE:
+		// function that combines the reallocation and cleaning of the buffer of the read out amount
+
 }
